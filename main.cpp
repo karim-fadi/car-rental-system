8443c8e4373682196133d0e10b2ae9fab3081218
toqaelmorshidy <toqaelmorshidy47@gmail.com>
2024-03-13 00:14:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,2 @@
 #include <iostream>
 #include<string>
-
-using namespace std;//yasaterrr
-
-
-int main()
-{
-	cout << "Hello" << endl;
-	cout << "Hello" << endl; 
-	system("pause");
-	return 0;
-}//huighb
