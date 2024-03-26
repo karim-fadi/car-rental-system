5c22efa206d1246f06fd9c29716a9497315c9c62
tiamohamedd <tiamohhamed@gmail.com>
2024-03-26 02:11:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-
 using namespace std;
-#include<iostream>
-using namespace std;
-
-
 int main()
 {
-	cout << "Hello" << endl;
-	cout << "Hello" << endl; 
-	system("pause");
-	return 0;
-} //Tianjfgnkldxlfkrfkrelfjregfjregjtrlgjonjfgnkldxlfkrfkrelfjregfjregjtrlgjo
+	const int size = 20;
+	int numbers[size],reverse[size];
+	cout << "enter 20 integers": << endl;
+	for (int i = 0; i < size; i++)
+		cin >> numbers[i];
+	for (int i = size - 1; i < 0; i--)
+		cout << "the numbers are" << " in reverse order " << reverse[i];
+
+
+}
