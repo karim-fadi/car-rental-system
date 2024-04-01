7ebfd0acdbfd2a3bb9b01b7900225fda83266894
toqaelmorshidy <toqaelmorshidy47@gmail.com>
2024-04-02 01:55:27 +0200

    added customer and car structs

--- main.cpp
+++ main.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+struct Customer {
+    string name;
+    int mobileNo;
+    string address;
+};
+
+struct Car {
+    string brand;
+    string model;
+    string color;
+    double distanceTraveled;
+    bool isRented; //Indicate whether the car is currently rented
+    Customer rentedBy; //the customer that is currently renting the car, If the car is not rented, this value will be null
+
+};
+
 
 
 int main()
 {
-	cout << "Hello" << endl;
-	cout << "Hello" << endl; 
-	system("pause");
-	return 0;
+    return 0;
 }
